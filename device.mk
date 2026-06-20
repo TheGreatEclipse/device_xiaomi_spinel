bcdae2d60ad3bbad15fd8c4701b1a9663dc5987a
The Great Eclipse <greateclipse609@gmail.com>
2026-06-20 10:04:21 +0400

    Partial upgrade

--- device.mk
+++ device.mk
@@ -1,91 +1,14 @@
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
-
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
-
-# API levels
-PRODUCT_SHIPPING_API_LEVEL := 35
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
-# Overlays
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
-# Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Product characteristics
-PRODUCT_CHARACTERISTICS := default
-
-# Rootdir
-PRODUCT_PACKAGES += \
-    capture.sh \
-    init.insmod.sh \
-    init.pstore_blk.sh \
-    playback.sh
-
-PRODUCT_PACKAGES += \
-    fstab.enableswap \
-    factory_init.connectivity.common.rc \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
-    init.aee.rc \
-    init.cgroup.rc \
-    init.connectivity.common.rc \
-    init.connectivity.rc \
-    init.modem.rc \
-    init.mt6789.rc \
-    init.mt6789.usb.rc \
-    init.mtkgki.rc \
-    init.project.rc \
-    init.sensor_2_0.rc \
-    init_connectivity.rc \
-    meta_init.connectivity.common.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    meta_init.vendor.rc \
-    multi_init.rc
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.enableswap:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.enableswap
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
-# Inherit the proprietary files
-$(call inherit-product, vendor/xiaomi/spinel/spinel-vendor.mk)
+# SELinux policy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/audio,$(TARGET_COPY_OUT_VENDOR)/etc/audio) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/bluetooth,$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/camera,$(TARGET_COPY_OUT_VENDOR)/etc/camera) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/display,$(TARGET_COPY_OUT_VENDOR)/etc/display) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/gps,$(TARGET_COPY_OUT_VENDOR)/etc/gps) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/input,$(TARGET_COPY_OUT_VENDOR)/etc/input) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc/media) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/sensors,$(TARGET_COPY_OUT_VENDOR)/etc/sensors) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/thermal,$(TARGET_COPY_OUT_VENDOR)/etc/thermal) \\
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/wifi,$(TARGET_COPY_OUT_VENDOR)/etc/wifi) \\
